e052f4658b2fde7272bf69b250c0865af332bd02
mslatour <sanderlatour@gmail.com>
2016-01-18 14:51:02 +0100

    Initial commit, setting boilerplate for angular application.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rssreaderClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rssreaderClientApp
+ */
+angular.module('rssreaderClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
